5edd26eddcb9f04e1c25ac704ebb1b8a9b9d2343
jaqubm <jaqobgm@gmail.com>
2023-05-27 21:47:51 +0200

    Fixed cart quantity of products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach(product => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -56,9 +56,18 @@ products.forEach(product => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+let cartQuantity = 0;
+
+cart.forEach((item) => {
+  cartQuantity += item.quantity;
+});
+
+document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
 document.querySelectorAll('.js-add-to-card').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
+    const productQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
 
     let matchingItem;
 
@@ -69,13 +78,21 @@ document.querySelectorAll('.js-add-to-card').forEach((button) => {
     });
 
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += productQuantity;
     }
     else {
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: productQuantity
       });
     }
+
+    cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
